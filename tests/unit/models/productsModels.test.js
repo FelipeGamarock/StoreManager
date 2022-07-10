88483c1023d9b89a4326bd3446a10ea2341b982f
Felipe Gama Danta <epilefgama2@gmail.com>
2022-07-10 12:35:24 -0300

    test: testa a productsModel, getById, getAll e Remove

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -4,22 +4,24 @@ const sinon = require('sinon');
 const connection = require('../../../models/connection');
 const productsModel = require('../../../models/productsModel');
 
-const productsMock = [
-  {
-    "id": 1,
-    "name": "Martelo de Thor"
-  },
-  {
-    "id": 2,
-    "name": "Traje de encolhimento"
-  },
-  {
-    "id": 3,
-    "name": "Escudo do Capitão América"
-  }
-]
-
 describe('models/productsModel', () => {
+  
+  describe('getAll', () => {
+    beforeEach(sinon.restore);
+    it('Dispara um erro ao não encontra o id solicitado', () => {
+      sinon.stub(connection, 'execute').rejects();
+      return chai.expect(productsModel.getAll(0)).to.eventually.be.rejected;
+    });
+    it('Não retorna nada caso a lista de produtos esteja vazia', () => {
+      sinon.stub(connection, 'execute').resolves([[]]);
+      return chai.expect(productsModel.getAll(0)).to.eventually.be.undefined;
+    });
+    it('Retorna um objeto ao encontrar o item na lista', () => {
+      sinon.stub(connection, 'execute').resolves([[{}]]);
+      return chai.expect(productsModel.getAll(0)).to.eventually.deep.equal({});
+    });
+  })
+  
   describe('getById', () => {
     beforeEach(sinon.restore);
     it('Dispara um erro ao não encontra o id solicitado', () => {
@@ -36,19 +38,16 @@ describe('models/productsModel', () => {
     });
   });
 
-  describe('getAll', () => {
+  describe('remove', () => {
     beforeEach(sinon.restore);
     it('Dispara um erro ao não encontra o id solicitado', () => {
       sinon.stub(connection, 'execute').rejects();
-      return chai.expect(productsModel.getAll(0)).to.eventually.be.rejected;
+      return chai.expect(productsModel.remove(1)).to.eventually.be.rejected;
     });
-    it('Não retorna nada caso a lista de produtos esteja vazia', () => {
-      sinon.stub(connection, 'execute').resolves([[]]);
-      return chai.expect(productsModel.getAll(0)).to.eventually.be.undefined;
+    it('Não retorna nada caso a remoção seja feita com sucesso', () => {
+      sinon.stub(connection, 'execute').resolves();
+      return chai.expect(productsModel.remove(1)).to.eventually.be.undefined;
     });
-    it('Retorna um objeto ao encontrar o item na lista', () => {
-      sinon.stub(connection, 'execute').resolves([[{}]]);
-      return chai.expect(productsModel.getAll(0)).to.eventually.deep.equal({});
-    });
-  })
+  });
+
 });
--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -1,43 +1,43 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const sinon = require('sinon');
+// const chai = require('chai');
+// const chaiAsPromised = require('chai-as-promised');
+// const sinon = require('sinon');
 
-const productsService = require('../../../services/productsService');
-const productsModel = require('../../../models/productsModel');
+// const productsService = require('../../../services/productsService');
+// const productsModel = require('../../../models/productsModel');
 
-chai.use(chaiAsPromised);
+// chai.use(chaiAsPromised);
 
 
-describe('models/productsModel', () => {
-  describe('getById', () => {
-    beforeEach(sinon.restore);
-    it('Dispara um erro ao não encontra o id solicitado', () => {
-      sinon.stub(productsModel, 'getById').rejects();
-      return chai.expect(productsService.getById(0)).to.eventually.be.rejected;
-    });
-    it('Não retorna nada caso a lista de produtos esteja vazia', () => {
-      sinon.stub(productsModel, 'getById').resolves([[]]);
-      return chai.expect(productsService.getById(0)).to.eventually.throw(Error);
-    });
-    it('Retorna um objeto ao encontrar o item na lista', () => {
-      sinon.stub(productsModel, 'getById').resolves([[{}]]);
-      return chai.expect(productsService.getById(0)).to.eventually.deep.equal({});
-    });
-  });
+// describe('models/productsModel', () => {
+//   describe('getById', () => {
+//     beforeEach(sinon.restore);
+//     it('Dispara um erro ao não encontra o id solicitado', () => {
+//       sinon.stub(productsModel, 'getById').rejects();
+//       return chai.expect(productsService.getById(0)).to.eventually.be.rejected;
+//     });
+//     it('Não retorna nada caso a lista de produtos esteja vazia', () => {
+//       sinon.stub(productsModel, 'getById').resolves([[]]);
+//       return chai.expect(productsService.getById(0)).to.eventually.throw(Error);
+//     });
+//     it('Retorna um objeto ao encontrar o item na lista', () => {
+//       sinon.stub(productsModel, 'getById').resolves([[{}]]);
+//       return chai.expect(productsService.getById(0)).to.eventually.deep.equal({});
+//     });
+//   });
 
-  describe('getAll', () => {
-    beforeEach(sinon.restore);
-    it('Dispara um erro caso a productModel lance um erro', () => {
-      sinon.stub(productsModel, 'getAll').rejects();
-      return chai.expect(productsService.getAll()).to.eventually.be.rejected;
-    });
-    it('Não retorna nada caso a lista de produtos esteja vazia', () => {
-      sinon.stub(productsModel, 'execute').resolves([[]]);
-      return chai.expect(productsService.getAll(0)).to.eventually.be.undefined;
-    });
-    it('Retorna um objeto ao encontrar o item na lista', () => {
-      sinon.stub(productsModel, 'execute').resolves([[{}]]);
-      return chai.expect(productsService.getAll(0)).to.eventually.deep.equal({});
-    });
-  })
-});
+//   describe('getAll', () => {
+//     beforeEach(sinon.restore);
+//     it('Dispara um erro caso a productModel lance um erro', () => {
+//       sinon.stub(productsModel, 'getAll').rejects();
+//       return chai.expect(productsService.getAll()).to.eventually.be.rejected;
+//     });
+//     it('Não retorna nada caso a lista de produtos esteja vazia', () => {
+//       sinon.stub(productsModel, 'execute').resolves([[]]);
+//       return chai.expect(productsService.getAll(0)).to.eventually.be.undefined;
+//     });
+//     it('Retorna um objeto ao encontrar o item na lista', () => {
+//       sinon.stub(productsModel, 'execute').resolves([[{}]]);
+//       return chai.expect(productsService.getAll(0)).to.eventually.deep.equal({});
+//     });
+//   })
+// });
